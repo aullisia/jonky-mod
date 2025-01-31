a2097661ea7940815b7aa466a0387961098f98f3
Aullisia <aullisia.dev@gmail.com>
2025-02-01 00:29:29 +0100

    Start Shield

--- src/main/java/jonky/modid/item/ModItems.java
+++ src/main/java/jonky/modid/item/ModItems.java
@@ -1,13 +1,12 @@
 package jonky.modid.item;
 
+import com.github.crimsondawn45.fabricshieldlib.lib.object.FabricShieldItem;
 import jonky.modid.Jonky;
 import jonky.modid.item.custom.BanknoteItem;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
@@ -16,6 +15,10 @@ import java.util.function.Function;
 
 public class ModItems {
     public static final Item BANKNOTE = registerItem("banknote", BanknoteItem::new, new Item.Settings());
+    public static final Item HEAVY_SHIELD = registerItem("heavy_shield",
+        (settings) -> new FabricShieldItem(settings, 10, 13, Items.NETHERITE_INGOT),
+        new Item.Settings().maxDamage(2500));
+
 
     public static Item registerItem(String path, Function<Item.Settings, Item> factory, Item.Settings settings) {
         final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Jonky.MOD_ID, path));
@@ -24,5 +27,9 @@ public class ModItems {
 
     public static void registerModItems() {
         Jonky.LOGGER.info("Registering Mod Items for" + Jonky.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
+            entries.add(HEAVY_SHIELD);
+        });
     }
 }
